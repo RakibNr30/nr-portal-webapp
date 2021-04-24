f86e4eaa358a0787a2be6859f869a1ca635ecb73
Abdur Rakib <rakib.cse14th@gmail.com>
2021-04-24 22:27:39 +0600

    some missing files added

---
+++ resources/js/components/ProjectMessageComposer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="text-center mt-4">
+        <h6 style="margin-bottom: 10px;">Send a message</h6>
+        <div class="row">
+            <div class="col-sm-9">
+                <textarea id="textarea" v-model="message" @keydown.enter="send" class="form-control mt-2" maxlength="225" rows="1" placeholder="Type your message...">
+                </textarea>
+            </div>
+            <div class="col-sm-3">
+                <button v-model="message" @click="send" class="btn btn-block btn-outline-success waves-effect waves-light mt-2">
+                    <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                    Send
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: ''
+            };
+        },
+        methods: {
+            send(e) {
+                e.preventDefault();
+                
+                if (this.message == '') {
+                    return;
+                }
+                this.$emit('send', this.message);
+                this.message = '';
+            }
+        }
+    };
+</script>
