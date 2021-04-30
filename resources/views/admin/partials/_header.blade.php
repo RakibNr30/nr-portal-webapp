b37bc1cd49871133710cb67ceddb0f77cca26f0c
Abdur Rakib <rakib.cse14th@gmail.com>
2021-04-30 20:12:54 +0600

    hamburger menu added.

--- resources/views/admin/partials/_header.blade.php
+++ resources/views/admin/partials/_header.blade.php
@@ -192,10 +192,10 @@
                     </a>
                 </div>
 
-              {{--  <button type="button" class="btn btn-sm px-3 font-size-16 header-item toggle-btn waves-effect"
+                <button type="button" class="btn btn-sm px-3 font-size-16 header-item toggle-btn waves-effect"
                         id="vertical-menu-btn">
                     <i class="fa fa-fw fa-bars"></i>
-                </button>--}}
+                </button>
 
                 {{--<form class="app-search d-none d-lg-inline-block">
                     <div class="position-relative">
